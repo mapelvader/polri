8201b9aaae1e1e2e1748493c0bb1cd97fc7ecfd4
wapbaru <155202658+mapelvader@users.noreply.github.com>
2024-11-04 20:29:01 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/main-home.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    // click target nav
+    $('a[href^="#"]').bind('click.smoothscroll', function(e) {
+        e.preventDefault();
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+});
+
+// e scroll anchor
+$(window).scroll(function() {
+    var scrollDistance = $(window).scrollTop();
+    $('.page-section').each(function(i) {
+        if ($(this).position().top <= scrollDistance + 200) {
+            $('.nav-menu a.active1').removeClass('active1');
+            $('.nav-menu a').eq(i).addClass('active1');
+        }
+    });
+}).scroll();
---
+++ js/main.js
@@ -0,0 +1,256 @@
+$(document).ready(function() {
+
+    // Toggle menu on click
+    $("#menu-toggler").click(function() {
+        toggleBodyClass("menu-active");
+    });
+    $(".menu-item").click(function() {
+        toggleBodyClass("menu-active");
+    });
+
+    $(".nav__link").click(function() {
+        $("body.menu-active").removeClass("menu-active");
+    });
+    $("#close1").click(function() {
+        $("body.menu-active").removeClass("menu-active");
+    });
+    $("#close2").click(function() {
+        $("body.menu-active").removeClass("menu-active");
+    });
+    $("#close3").click(function() {
+        $("body.menu-active").removeClass("menu-active");
+    });
+    $("#close4").click(function() {
+        $("body.menu-active").removeClass("menu-active");
+    });
+    $("#close5").click(function() {
+        $("body.menu-active").removeClass("menu-active");
+    });
+
+
+    function toggleBodyClass(className) {
+        document.body.classList.toggle(className);
+    }
+
+    // nav fixed
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 150) {
+            $(".header").addClass("fixshow");
+            $(".main").addClass("spacetop");
+            $(".logo").addClass("logo-show");
+        } else {
+            $(".header").removeClass("fixshow");
+            $(".main").removeClass("spacetop");
+            $(".logo").removeClass("logo-show");
+        }
+    });
+
+    $('.icon-sosmed').click(function() {
+        if ($('.social').hasClass('social-show')) {
+            $('.social').removeClass('social-show');
+        } else {
+            $('.social').addClass('social-show');
+        }
+    });
+
+    // s backtotop
+    var toggleHeight = $(window).outerHeight() * 0.5;
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > toggleHeight) {
+            $(".m-backtotop").addClass("active");
+        } else {
+            $(".m-backtotop").removeClass("active");
+        }
+    });
+
+    $(".m-backtotop").click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, "slow");
+        return false;
+    });
+    // e backtotop
+});
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        width: '100%',
+        videoId: 'Z5p6f6ic99g',
+        playerVars: {
+            'autoplay': 1,
+            'playsinline': 1,
+            'loop': 1,
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.mute();
+    event.target.playVideo();
+}
+var done = true;
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.ENDED) {
+        player.playVideo();
+    }
+
+}
+
+var $swiper = $(".slide1");
+var $bottomSlide = null;
+var $bottomSlideContent = null;
+
+var mySwiper = new Swiper(".slide1", {
+
+    spaceBetween: 1,
+    slidesPerView: 3,
+    centeredSlides: true,
+    roundLengths: true,
+    loop: true,
+    loopAdditionalSlides: 30,
+    speed: 1000,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// s parallax
+$.fn.moveIt = function() {
+    var $window = $(window);
+    var instances = [];
+
+    $(this).each(function() {
+        instances.push(new moveItItem($(this)));
+    });
+
+    window.addEventListener('scroll', function() {
+        var scrollTop = $window.scrollTop();
+        instances.forEach(function(inst) {
+            inst.update(scrollTop);
+        });
+    }, { passive: true });
+}
+
+var moveItItem = function(el) {
+    this.el = $(el);
+    this.speed = parseInt(this.el.attr('data-scroll-speed'));
+};
+
+moveItItem.prototype.update = function(scrollTop) {
+    this.el.css('transform', 'translateY(' + (scrollTop / this.speed) + 'px)');
+};
+
+$(function() {
+    $('[data-scroll-speed]').moveIt();
+});
+
+$(window).scroll(function() {
+    var scrollTop = $(window).scrollTop();
+    console.log("scrollTop>>>" + scrollTop);
+    if (scrollTop == 0) {
+        $("#scrollFx").css({ "bottom": "-1000px" });
+    } else {
+        $("#scrollFx").css({ "bottom": ($(window).scrollTop()) + "px" });
+    }
+});
+
+// video
+function videoId(button) {
+    var $videoUrl = button.attr("data-video");
+    if ($videoUrl !== undefined) {
+        var $videoUrl = $videoUrl.toString();
+        var srcVideo;
+
+        if ($videoUrl.indexOf("mp4") !== -1) {
+            return (
+                '<video loop playsinline autoplay controls><source src="' +
+                $videoUrl +
+                '" type="video/mp4"></video>'
+            );
+        } else {
+            alert(
+                "The video assigned is not from Youtube, Vimeo or MP4, remember to enter the correct complete video link .\n - Youtube: https://www.youtube.com/watch?v=43ngkc2Ejgw\n - Vimeo: https://vimeo.com/111939668\n - MP4 https://server.com/file.mp4"
+            );
+            return false;
+        }
+
+    } else {
+        alert("No video assigned.");
+        return false;
+    }
+}
+
+$(".lets-play").click(function(e) {
+    e.preventDefault();
+    var $theVideo = videoId($(this));
+    if ($theVideo) {
+        $("#video-wrap")
+            .html(
+                '<span class="video-overlay"></span><div class="video-container">' +
+                $theVideo +
+                '</div><button class="close-video">x</button>'
+            )
+            .addClass("active");
+    }
+});
+
+$(document).on("click", ".close-video, .video-overlay", function() {
+    $("#video-wrap").empty().removeClass("active");
+});
+
+const scroll = document.querySelector(".scroll");
+var isDown = false;
+var scrollX;
+var scrollLeft;
+
+scroll.addEventListener("mouseup", () => {
+    isDown = false;
+    scroll.classList.remove("active");
+});
+
+
+scroll.addEventListener("mouseleave", () => {
+    isDown = false;
+    scroll.classList.remove("active");
+});
+
+
+scroll.addEventListener("mousedown", e => {
+    e.preventDefault();
+    isDown = true;
+    scroll.classList.add("active");
+    scrollX = e.pageX - scroll.offsetLeft;
+    scrollLeft = scroll.scrollLeft;
+});
+
+scroll.addEventListener("mousemove", e => {
+    if (!isDown) return;
+    e.preventDefault();
+    var element = e.pageX - scroll.offsetLeft;
+    var scrolling = (element - scrollX) * 2;
+    scroll.scrollLeft = scrollLeft - scrolling;
+});
